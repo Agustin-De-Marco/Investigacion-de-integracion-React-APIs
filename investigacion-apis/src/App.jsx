95c0323ae58927113d136e48beea2496c3164a28
Agustin-De-Marco <48796010@est.ort.edu.ar>
2025-06-17 08:31:48 -0300

    final commit

--- investigacion-apis/src/App.jsx
+++ investigacion-apis/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ProductList from './components/ProductList';
-import './App.css'; // Puedes limpiar el contenido de este archivo si quieres
 
 function App() {
   return (
